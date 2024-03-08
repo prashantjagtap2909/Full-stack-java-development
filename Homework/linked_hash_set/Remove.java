75d3b3869a6493c03b691d77fcd78ef235c41bd8
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-03-08 09:54:07 +0530

    Added LinkedHashSet

---
+++ Homework/linked_hash_set/Remove.java
@@ -0,0 +1,20 @@
+package com.linked_hash_set;
+
+
+import java.util.LinkedHashSet;
+
+public class Remove {
+	public static void main(String[] args) {
+		LinkedHashSet arr = new LinkedHashSet();
+		arr.add(20);
+		arr.add(15);
+		arr.add(26);
+		arr.add(51);
+		arr.add(9);
+		arr.add(67);
+		
+		
+		System.out.println(arr);
+		System.out.println(arr.remove(2));
+	}
+}
