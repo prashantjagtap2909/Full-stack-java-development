3429526ad33bbd16fdaa6472d53450ca98970f6f
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-03-08 09:52:11 +0530

    Added TreeSet

---
+++ Homework/Collection/com/tree_set/Empty.java
@@ -0,0 +1,20 @@
+package com.tree_set;
+
+
+import java.util.TreeSet;
+
+public class Empty {
+
+	public static void main(String[] args) {
+		TreeSet arr = new TreeSet();
+		arr.add(20);
+		arr.add(15);
+		arr.add(26);
+		arr.add(51);
+		arr.add(9);
+		arr.add(67);
+		
+		System.out.println(arr);
+		System.out.println(arr.isEmpty());
+	}
+}
